4a79fc3d421b2af8ffa30d42fede51477b103243
minaripenguin <minaripenguin@users.noreply.github.com>
2024-02-02 12:33:16 +0900

    fixup! SystemUI: Implement Island notification feature
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5863,6 +5863,11 @@ public final class Settings {
         public static final String ISLAND_NOTIFICATION = "island_notification";
 
         /**
+         * @hide
+         */
+        public static final String ISLAND_NOTIFICATION_NOW_PLAYING = "island_notification_now_playing";
+
+        /**
          * These are all public system settings
          *
          * @hide
