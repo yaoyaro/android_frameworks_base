52542cdf0abb4b313c824b5da5fed18605da7a9d
Shenglei Mao <shenglei@google.com>
2024-02-21 10:34:51 -0800

    Allow voice interaction service to be started even if voice recognition service is null.
    Bug: 324473159
    Test: manually upgrade assistant apk and observe that voice recognition
          service is correctly bound.
    Change-Id: Ie0b1b1893ace3b14f2a1c1ed9af38267940356db

--- services/voiceinteraction/java/com/android/server/voiceinteraction/VoiceInteractionManagerService.java
+++ services/voiceinteraction/java/com/android/server/voiceinteraction/VoiceInteractionManagerService.java
@@ -2474,7 +2474,6 @@ public class VoiceInteractionManagerService extends SystemService {
                         if (anyPackagesAppearing()) {
                             initRecognizer(userHandle);
                         }
-                        return;
                     }
 
                     if (curInteractor != null) {
@@ -2524,15 +2523,16 @@ public class VoiceInteractionManagerService extends SystemService {
                         }
                     }
 
-                    // There is no interactor, so just deal with a simple recognizer.
-                    int change = isPackageDisappearing(curRecognizer.getPackageName());
-                    if (change == PACKAGE_PERMANENT_CHANGE
-                            || change == PACKAGE_TEMPORARY_CHANGE) {
-                        setCurRecognizer(findAvailRecognizer(null, userHandle), userHandle);
+                    if (curRecognizer != null) {
+                        int change = isPackageDisappearing(curRecognizer.getPackageName());
+                        if (change == PACKAGE_PERMANENT_CHANGE
+                                || change == PACKAGE_TEMPORARY_CHANGE) {
+                            setCurRecognizer(findAvailRecognizer(null, userHandle), userHandle);
 
-                    } else if (isPackageModified(curRecognizer.getPackageName())) {
-                        setCurRecognizer(findAvailRecognizer(curRecognizer.getPackageName(),
-                                userHandle), userHandle);
+                        } else if (isPackageModified(curRecognizer.getPackageName())) {
+                            setCurRecognizer(findAvailRecognizer(curRecognizer.getPackageName(),
+                                    userHandle), userHandle);
+                        }
                     }
                 }
             }
