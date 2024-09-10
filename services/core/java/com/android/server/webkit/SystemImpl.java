3cb263057eeff30cab33c296088a78afcf6f9bf3
Jaida Wu <mlgmxyysd@meowcat.org>
2024-09-10 09:33:41 +0800

    base: Add support for webview implementation selection
    Allow users to choose a WebView implementation. Choosing an implementation will result in different functionality, but may lead to compatibility or security issues.
    
    Feature: yt/KSCOPE-45
    Signed-off-by: Jaida Wu <mlgmxyysd@meowcat.org>
    Change-Id: If491a859aa4ebb66f09e707175f429cd87216cff
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: fukiame <fukiame@proton.me>

--- services/core/java/com/android/server/webkit/SystemImpl.java
+++ services/core/java/com/android/server/webkit/SystemImpl.java
@@ -19,6 +19,7 @@ package com.android.server.webkit;
 import android.app.ActivityManager;
 import android.app.AppGlobals;
 import android.content.Context;
+import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
@@ -73,10 +74,12 @@ public class SystemImpl implements SystemInterface {
     private SystemImpl() {
         int numFallbackPackages = 0;
         int numAvailableByDefaultPackages = 0;
+        Context mContext = AppGlobals.getInitialApplication();
         XmlResourceParser parser = null;
         List<WebViewProviderInfo> webViewProviders = new ArrayList<WebViewProviderInfo>();
+        List<String> webviewPackages = new ArrayList<>();
         try {
-            parser = AppGlobals.getInitialApplication().getResources().getXml(
+            parser = mContext.getResources().getXml(
                     com.android.internal.R.xml.config_webview_packages);
             XmlUtils.beginDocument(parser, TAG_START);
             while(true) {
@@ -118,6 +121,7 @@ public class SystemImpl implements SystemInterface {
                         numAvailableByDefaultPackages++;
                     }
                     webViewProviders.add(currentProvider);
+                    webviewPackages.add(packageName);
                 }
                 else {
                     Log.e(TAG, "Found an element that is not a WebView provider");
@@ -131,6 +135,15 @@ public class SystemImpl implements SystemInterface {
         if (numAvailableByDefaultPackages == 0) {
             throw new AndroidRuntimeException("There must be at least one WebView package "
                     + "that is available by default");
+        }
+        PackageManager pm = mContext.getPackageManager();
+        List<ApplicationInfo> appList = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+        for (ApplicationInfo appInfo : appList) {
+            if (WebViewFactory.getWebViewLibrary(appInfo) != null
+                    && !webviewPackages.contains(appInfo.packageName)) {
+                webViewProviders.add(new WebViewProviderInfo(appInfo.packageName,
+                        appInfo.loadLabel(pm).toString(), false, false, null));
+            }
         }
         mWebViewProviderPackages =
                 webViewProviders.toArray(new WebViewProviderInfo[webViewProviders.size()]);
