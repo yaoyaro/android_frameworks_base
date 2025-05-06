0aea9bee441ebbec293a0e9dcba35614521cd346
EnBiao.Su <suenbiao@oppo.corp-partner.google.com>
2025-05-06 16:56:50 +0800

    Fix only kill current user proc while clear data
    While clearApplicationUserData for a specified user, only the processes under that user should be killed
    
    Test:
      1.create a new user
      2.launch appA under user 0
      3.launch appA under the newly created user
      4.clear appA's user data within user_new
      5.ensure appA is not killed in user 0
    
    Bug: 414516353
    Google: 3619009
    Change-Id: I5a004c52aaac5b16ccda137a3c007d53ac470fd6

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -4825,7 +4825,7 @@ public class PackageManagerService implements PackageSender, TestUtilityService 
                 public void run() {
                     mHandler.removeCallbacks(this);
                     final boolean succeeded;
-                    try (PackageFreezer freezer = freezePackage(packageName, UserHandle.USER_ALL,
+                    try (PackageFreezer freezer = freezePackage(packageName, userId,
                             "clearApplicationUserData",
                             ApplicationExitInfo.REASON_USER_REQUESTED)) {
                         synchronized (mInstallLock) {
