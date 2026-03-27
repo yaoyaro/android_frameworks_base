2780789f881743718eefd2c77bafa13a89509fb8
fukiame <fukiame@proton.me>
2026-03-27 18:23:39 +0700

    [DNM] Revert "Don't allow 3p apps to become voice recognizer automatically"
    user installed cts apk
    TODO find a way to reapply this
    
    This reverts commit 641e0504cb214df74b2b6d882b6d2bfa3d79c2d7.
    
    Signed-off-by: fukiame <fukiame@proton.me>

--- services/voiceinteraction/java/com/android/server/voiceinteraction/VoiceInteractionManagerService.java
+++ services/voiceinteraction/java/com/android/server/voiceinteraction/VoiceInteractionManagerService.java
@@ -861,20 +861,12 @@ public class VoiceInteractionManagerService extends SystemService {
                         }
                     }
                 }
-
-                // If prefPackage isn't found then only default to system recognizer.
-                // prefPackage could be either the current recognizer or the default recognizer.
-                for (int i = 0; i < numAvailable; i++) {
-                    ServiceInfo serviceInfo = available.get(i).getServiceInfo();
-                    if ((serviceInfo.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) == 0) {
-                        continue;
-                    }
-                    return new ComponentName(serviceInfo.packageName, serviceInfo.name);
+                if (numAvailable > 1) {
+                    Slog.w(TAG, "more than one voice recognition service found, picking first");
                 }
 
-                Slog.w(TAG, "no auto selectable voice recognition services found for user "
-                        + userHandle);
-                return null;
+                ServiceInfo serviceInfo = available.get(0).getServiceInfo();
+                return new ComponentName(serviceInfo.packageName, serviceInfo.name);
             }
         }
 
