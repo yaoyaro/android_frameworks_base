c407a9f1e280d7f241e45374f4e2c5ca33451c69
fukiame <fukiame@proton.me>
2026-03-29 15:32:16 +0700

    Revert "Spoof a valid certificate chain for hardware attestations"
    This reverts commit b0858172d4a9415e25fc2a0f406d7deddbf65e1e.

--- keystore/java/android/security/KeyStore2.java
+++ keystore/java/android/security/KeyStore2.java
@@ -31,8 +31,6 @@ import android.system.keystore2.KeyEntryResponse;
 import android.system.keystore2.ResponseCode;
 import android.util.Log;
 
-import com.android.internal.util.pb.AttestationHooks;
-
 import java.util.Calendar;
 import java.util.Objects;
 
@@ -261,9 +259,7 @@ public class KeyStore2 {
      */
     public KeyEntryResponse getKeyEntry(@NonNull KeyDescriptor descriptor)
             throws KeyStoreException {
-        KeyEntryResponse response = handleRemoteExceptionWithRetry((service) -> service.getKeyEntry(descriptor));
-
-        return AttestationHooks.onGetKeyEntry(response);
+        return handleRemoteExceptionWithRetry((service) -> service.getKeyEntry(descriptor));
     }
 
     /**
