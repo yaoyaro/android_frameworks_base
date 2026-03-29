e0bf4d639fdc1d2f8d148dda2095de3505c2a360
fukiame <fukiame@proton.me>
2026-03-29 17:50:19 +0700

    DataSwitchTile: open normal cellular settings
    Signed-off-by: fukiame <fukiame@proton.me>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/DataSwitchTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/DataSwitchTile.java
@@ -174,7 +174,7 @@ public class DataSwitchTile extends QSTileImpl<BooleanState> {
 
     @Override
     public Intent getLongClickIntent() {
-        return new Intent(Settings.Panel.ACTION_MOBILE_DATA);
+        return getCellularSettingIntent();
     }
 
     @Override
@@ -286,5 +286,15 @@ public class DataSwitchTile extends QSTileImpl<BooleanState> {
             }
         }
         return mInitialState;
+    }
+
+    static Intent getCellularSettingIntent() {
+        Intent intent = new Intent(Settings.ACTION_NETWORK_OPERATOR_SETTINGS);
+        int dataSub = SubscriptionManager.getDefaultDataSubscriptionId();
+        if (dataSub != SubscriptionManager.INVALID_SUBSCRIPTION_ID) {
+            intent.putExtra(Settings.EXTRA_SUB_ID,
+                    SubscriptionManager.getDefaultDataSubscriptionId());
+        }
+        return intent;
     }
 }
