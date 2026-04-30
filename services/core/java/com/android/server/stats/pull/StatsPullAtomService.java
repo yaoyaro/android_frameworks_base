5b1924e5a740e68e6f19248b98c9565b50695ffc
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-05-01 06:29:14 +0800

    [ax_perf] disabling dma buff stats pull
    simpleperf shows high cpu usage caused by dma stats pull during app launch, we dont care about dma buf stats, only enable it on eng builds
    
    Change-Id: Ifa76e5d0cfbc552a8257b43450e332d49c9c3ad8

--- services/core/java/com/android/server/stats/pull/StatsPullAtomService.java
+++ services/core/java/com/android/server/stats/pull/StatsPullAtomService.java
@@ -2444,6 +2444,9 @@ public class StatsPullAtomService extends SystemService {
     }
 
     private void registerProcessDmabufMemory() {
+        if (!Build.IS_ENG) {
+            return;
+        }
         int tagId = FrameworkStatsLog.PROCESS_DMABUF_MEMORY;
         mStatsManager.setPullAtomCallback(
                 tagId,
@@ -2454,6 +2457,9 @@ public class StatsPullAtomService extends SystemService {
     }
 
     int pullProcessDmabufMemory(int atomTag, List<StatsEvent> pulledData) {
+        if (!Build.IS_ENG) {
+            return StatsManager.PULL_SKIP;
+        }
         KernelAllocationStats.ProcessDmabuf[] procBufs =
                 KernelAllocationStats.getDmabufAllocations();
 
