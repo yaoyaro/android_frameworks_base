fe743e7128570b533449a6bdb8f09b439c32f611
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-05-07 08:03:13 +0800

    [ax_perf] caching phonewindowmanager setup check
    settings call takes up to 30% of the onInputEvent cpu usage, add a caching mechanism to avoid repeated binder ipcs
    
      10.79%    0.00%  android.ui       1558  __start_thread
             |
             -- __start_thread
                |
                 -- __pthread_start(void*) (.__uniq.67847048707805468364044055584648682506)
                     |
                     |--96.65%-- android.os.MessageQueue.nextConcurrent
                     |    |
                     |    |--98.17%-- art_jni_trampoline
                     |    |           android::android_os_MessageQueue_nativePollOnce(_JNIEnv*, _jobject*, long, int)
                     |    |           android::Looper::pollOnce(int, int*, int*, void**)
                     |    |    |
                     |    |    |--96.13%-- android::NativeInputEventReceiver::handleEvent(int, int, void*)
                     |    |    |           android::NativeInputEventReceiver::consumeEvents(_JNIEnv*, bool, long, bool*)
                     |    |    |    |
                     |    |    |    |--92.66%-- _JNIEnv::CallVoidMethod(_jobject*, _jmethodID*, ...)
                     |    |    |    |           art_quick_invoke_stub
                     |    |    |    |    |
                     |    |    |    |    |--56.84%-- android.view.InputEventReceiver.onBatchedInputEventPending
                     |    |    |    |    |           android::NativeInputEventReceiver::consumeEvents(_JNIEnv*, bool, long, bool*)
                     |    |    |    |    |    |
                     |    |    |    |    |    |--60.70%-- _JNIEnv::CallVoidMethod(_jobject*, _jmethodID*, ...)
                     |    |    |    |    |    |           android.view.InputEventReceiver.dispatchInputEvent
                     |    |    |    |    |    |           com.android.server.wm.PointerEventDispatcher.onInputEvent
                     |    |    |    |    |    |    |
                     |    |    |    |    |    |    |--58.35%-- com.android.server.wm.SystemGesturesPointerEventListener.onPointerEvent
                     |    |    |    |    |    |    |    |
                     |    |    |    |    |    |    |    |--54.08%-- com.android.server.wm.DisplayPolicy$1.onSwipeFromRight
                     |    |    |    |    |    |    |    |           NterpCommonInvokeInstance
                     |    |    |    |    |    |    |    |    |
                     |    |    |    |    |    |    |    |    |--61.67%-- com.android.server.wm.DisplayPolicy.requestTransientBars
                     |    |    |    |    |    |    |    |    |           NterpCommonInvokeInterface
                     |    |    |    |    |    |    |    |    |           com.android.server.policy.PhoneWindowManager.isUserSetupComplete
                     |    |    |    |    |    |    |    |    |           NterpCommonInvokeStatic
                     |    |    |    |    |    |    |    |    |           android.provider.Settings$Secure.getIntForUser
                     |    |    |    |    |    |    |    |    |           android.provider.Settings$Secure.getStringForUser
                     |    |    |    |    |    |    |    |    |           android.provider.Settings$NameValueCache.getStringForUser
                     |    |    |    |    |    |    |    |    |           android.content.ContentProvider$Transport.call
                     |    |    |    |    |    |    |    |    |           android.content.ContentProvider.call
                     |    |    |    |    |    |    |    |    |           com.android.providers.settings.SettingsProvider.call
                     |    |    |    |    |    |    |    |    |           com.android.providers.settings.SettingsProvider.getSecureSetting
                     |    |    |    |    |    |    |    |    |           com.android.providers.settings.SettingsProvider$SettingsRegistry.getSettingLocked
                     |    |    |    |    |    |    |    |    |           android.util.ArrayMap.indexOf
    
    Change-Id: I805678e4334011473b7a5ad84a2de7bb019c7df9

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -466,6 +466,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private boolean mHasFeatureWatch;
     private boolean mHasFeatureLeanback;
     private boolean mHasFeatureHdmiCec;
+    
+    private boolean mIsSetupComplete = false;
 
     // Assigned on main thread, accessed on UI thread
     volatile VrManagerInternal mVrManagerInternal;
@@ -888,6 +890,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         void observe() {
             // Observe all users' changes
             ContentResolver resolver = mContext.getContentResolver();
+            resolver.registerContentObserver(Settings.Secure.getUriFor(
+                    Settings.Secure.USER_SETUP_COMPLETE), false, this,
+                    UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.END_BUTTON_BEHAVIOR), false, this,
                     UserHandle.USER_ALL);
@@ -1903,8 +1908,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
     @Override
     public boolean isUserSetupComplete() {
-        boolean isSetupComplete = Settings.Secure.getIntForUser(mContext.getContentResolver(),
-                Settings.Secure.USER_SETUP_COMPLETE, 0, UserHandle.USER_CURRENT) != 0;
+        boolean isSetupComplete = mIsSetupComplete;
         if (mHasFeatureLeanback) {
             isSetupComplete &= isTvUserSetupComplete();
         } else if (mHasFeatureAuto) {
@@ -3011,6 +3015,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
     public void updateSettings() {
         ContentResolver resolver = mContext.getContentResolver();
+        mIsSetupComplete = Settings.Secure.getIntForUser(mContext.getContentResolver(),
+                Settings.Secure.USER_SETUP_COMPLETE, 0, UserHandle.USER_CURRENT) != 0;
         boolean updateRotation = false;
         int mDeviceHardwareWakeKeys = mContext.getResources().getInteger(
                 org.lineageos.platform.internal.R.integer.config_deviceHardwareWakeKeys);
