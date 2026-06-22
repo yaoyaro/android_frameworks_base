5678c9c1b5cb22e267a53b298d2b75fe5ac292ef
fukiame <fukiame@proton.me>
2026-06-22 17:00:21 +0700

    Revert "core: jni: add a separate prop to activate ART LowMemoryMode"
    applied twice
    
    This reverts commit 7a2d71b66d75fb5f89b9f36ac13622180d97dc9e.

--- core/jni/AndroidRuntime.cpp
+++ core/jni/AndroidRuntime.cpp
@@ -908,11 +908,6 @@ int AndroidRuntime::startVm(JavaVM** pJavaVM, JNIEnv** pEnv, bool zygote, bool p
       addOption("-XX:LowMemoryMode");
     }
 
-    property_get("ro.config.art_lowmem", propBuf, "");
-    if (strcmp(propBuf, "true") == 0) {
-      addOption("-XX:LowMemoryMode");
-    }
-
     /*
      * Garbage-collection related options.
      */
